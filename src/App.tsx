583d66ec238ee2a01871b96172339ad427480b16
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-03-07 20:37:36 +0200

    Update App component text to test the pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
     <div className="font-bold text-4xl text-red-300">
-      This is just some styled text
+      Cool Cars Garage Coming soon...
     </div>
   )
 }
