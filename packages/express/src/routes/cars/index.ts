d58f73612d3c3e8494686d6d67af536f49203807
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-08-13 23:23:38 +0300

    Update the styling for the pdf exports

--- packages/express/src/routes/cars/index.ts
+++ packages/express/src/routes/cars/index.ts
@@ -52,7 +52,7 @@ carsRouter.post("/export", cors(), async (req, res) => {
     const { carsIds, startDate, endDate, reportId } = req.body
     const pdfDoc = new PDFDoc()
     pdfDoc.pipe(res)
-    pdfDoc.fontSize(14).text("Cool Cars South Coast Ltd.")
+    pdfDoc.fontSize(11).text("Cool Cars South Coast Ltd.")
     pdfDoc.moveDown()
 
     const toTimestamp = (date?: Timestamp) => {
@@ -81,7 +81,7 @@ carsRouter.post("/export", cors(), async (req, res) => {
 
       const { interior, exterior } = questionsData as QuestionDoc
 
-      pdfDoc.fontSize(20).text(
+      pdfDoc.fontSize(16).text(
         `${reportData.vehicleRegNumber} ${reportData.driverName} ${reportData.timestamp
           .toDate()
           .toLocaleString("en-GB", {
@@ -94,17 +94,32 @@ carsRouter.post("/export", cors(), async (req, res) => {
         { align: "center" }
       )
       pdfDoc.moveDown()
-
-      pdfDoc.fontSize(14)
+      pdfDoc.fontSize(11)
+      pdfDoc.text(`Odometer reading: ${reportData.odoReading} km`, {
+        align: "center"
+      })
+      pdfDoc.moveDown()
 
       const table = pdfDoc.table({
-        columnStyles: { borderColor: "gray", border: 0.5 }
+        columnStyles: [100, "*", 100]
       })
 
       table.row([
-        { text: "Category", align: "center" },
-        { text: "Question", align: "center" },
-        { text: "Status", align: "center" }
+        {
+          text: "Category",
+          align: "center",
+          borderColor: "#f4f4f4"
+        },
+        {
+          text: "Question",
+          align: "center",
+          borderColor: "#f4f4f4"
+        },
+        {
+          text: "Status",
+          align: "center",
+          borderColor: "#f4f4f4"
+        }
       ])
 
       interior.forEach((question, index) => {
@@ -112,9 +127,24 @@ carsRouter.post("/export", cors(), async (req, res) => {
           f => f.questionIndex === index && f.questionCategory === "interior"
         )
         table.row([
-          { text: "Interior", align: "center" },
-          { text: question.label, align: "center" },
-          { text: fault ? fault.status : "passed", align: "center" }
+          {
+            text: "Interior",
+            align: "center",
+            borderColor: "#f4f4f4"
+          },
+          {
+            text: question.label,
+            align: "center",
+            borderColor: "#f4f4f4",
+            textOptions: {
+              lineBreak: true
+            }
+          },
+          {
+            text: fault ? fault.status : "passed",
+            align: "center",
+            borderColor: "#f4f4f4"
+          }
         ])
       })
 
@@ -123,9 +153,21 @@ carsRouter.post("/export", cors(), async (req, res) => {
           f => f.questionIndex === index && f.questionCategory === "exterior"
         )
         table.row([
-          { text: "Exterior", align: "center" },
-          { text: question.label, align: "center" },
-          { text: fault ? fault.status : "passed", align: "center" }
+          {
+            text: "Exterior",
+            align: "center",
+            borderColor: "#f4f4f4"
+          },
+          {
+            text: question.label,
+            align: "center",
+            borderColor: "#f4f4f4"
+          },
+          {
+            text: fault ? fault.status : "passed",
+            align: "center",
+            borderColor: "#f4f4f4"
+          }
         ])
       })
 
@@ -150,36 +192,45 @@ carsRouter.post("/export", cors(), async (req, res) => {
 
     const carsData = carsSnapshot.docs.map(doc => doc.data()) as Report[]
 
-    pdfDoc.fontSize(20).text("Vehicle Check Report", { align: "center" })
+    pdfDoc.fontSize(18).text("Vehicle Check Report", { align: "center" })
     pdfDoc.moveDown()
 
     const table = pdfDoc.table()
-    table.row([
-      "Status",
-      "Vehicle Registration",
-      "Date and Time",
-      "Driver",
-      "Odometer reading",
-      "Faults"
-    ])
+    pdfDoc.fontSize(11)
+    table.row(
+      [
+        "Status",
+        "Vehicle Registration",
+        "Date and Time",
+        "Driver",
+        "Odometer reading",
+        "Faults"
+      ].map(value => ({ text: value, align: "center", borderColor: "#f4f4f4" }))
+    )
 
     carsData.forEach(car => {
-      table.row([
-        car.fault?.some(f => f.status === "pending")
-          ? "Defects found"
-          : "Passed",
-        car.vehicleRegNumber,
-        car.timestamp.toDate().toLocaleString(undefined, {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-          hour: "2-digit",
-          minute: "2-digit"
-        }),
-        car.driverName,
-        car.odoReading.toString(),
-        (car.fault?.length || 0).toString()
-      ])
+      table.row(
+        [
+          car.fault?.some(f => f.status === "pending")
+            ? "Defects found"
+            : "Passed",
+          car.vehicleRegNumber,
+          car.timestamp.toDate().toLocaleString(undefined, {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit"
+          }),
+          car.driverName,
+          car.odoReading.toString(),
+          (car.fault?.length || 0).toString()
+        ].map(value => ({
+          text: value,
+          align: "center",
+          borderColor: "#f4f4f4"
+        }))
+      )
     })
 
     pdfDoc.end()
