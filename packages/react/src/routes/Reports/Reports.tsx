01f57c0e770ee23657a6ab1c8cb7520863862da4
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-09-04 10:45:01 +0300

    Fixed displayed date for the reports list items

--- packages/react/src/routes/Reports/Reports.tsx
+++ packages/react/src/routes/Reports/Reports.tsx
@@ -67,7 +67,8 @@ const metadataConfig: Record<
         month: "2-digit",
         day: "2-digit",
         hour: "2-digit",
-        minute: "2-digit"
+        minute: "2-digit",
+        timeZone: "Europe/Bucharest"
       })
   }
 }
