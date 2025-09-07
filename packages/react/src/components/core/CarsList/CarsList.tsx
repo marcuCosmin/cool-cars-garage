b9e5122a743b372c12a85e12d7506d97caf838b2
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-09-07 22:28:48 +0300

    Comment some components that are currently causing build issues

--- packages/react/src/components/core/CarsList/CarsList.tsx
+++ packages/react/src/components/core/CarsList/CarsList.tsx
@@ -1,80 +1,82 @@
-import { useEffect } from "react"
+// import { useEffect } from "react"
 
-import { useAppDispatch, useAppSelector } from "@/redux/config"
-import { openModal } from "@/redux/modalSlice"
-import { deleteCar, fetchAllCars, setEditedCarId } from "@/redux/carsSlice"
+// import { useAppDispatch, useAppSelector } from "@/redux/config"
+// import { openModal } from "@/redux/modalSlice"
+// import { deleteCar, fetchAllCars, setEditedCarId } from "@/redux/carsSlice"
 
-import type { FiltersConfig } from "@/components/core/DataView/DataView.model"
-import { DataView } from "@/components/core/DataView/DataView"
+// import type { FiltersConfig } from "@/components/core/DataView/DataView.model"
+// import { DataView } from "@/components/core/DataView/DataView"
 
-import { Loader } from "@/components/basic/Loader"
+// import { Loader } from "@/components/basic/Loader"
 
-import { getCheckpointFilter } from "./CarsList.utils"
+// import { getCheckpointFilter } from "./CarsList.utils"
 
-import type { CarItemData } from "./CarsList.model"
+// import type { CarItemData } from "./CarsList.model"
 
-const filtersConfig: FiltersConfig<CarItemData> = {
-  "Council": {
-    field: "council",
-    type: "select",
-    options: ["Wolverhampton", "Cornwall", "PSV", "Portsmouth", "Other"]
-  },
-  "MOT expires soon": {
-    type: "toggle",
-    filterFn: getCheckpointFilter("mot")
-  },
-  "Road Tax expires soon": {
-    type: "toggle",
-    filterFn: getCheckpointFilter("roadTax")
-  }
-}
+// const filtersConfig: FiltersConfig<CarItemData> = {
+//   "Council": {
+//     field: "council",
+//     type: "select",
+//     options: ["Wolverhampton", "Cornwall", "PSV", "Portsmouth", "Other"]
+//   },
+//   "MOT expires soon": {
+//     type: "toggle",
+//     filterFn: getCheckpointFilter("mot")
+//   },
+//   "Road Tax expires soon": {
+//     type: "toggle",
+//     filterFn: getCheckpointFilter("roadTax")
+//   }
+// }
 
-export const CarsList = () => {
-  const { cars, metadata } = useAppSelector(state => state.carsReducer)
-  const dispatch = useAppDispatch()
+// export const CarsList = () => {
+//   const { cars, metadata } = useAppSelector(state => state.carsReducer)
+//   const dispatch = useAppDispatch()
 
-  const initialData: Record<string, CarItemData> = Object.entries(cars).reduce(
-    (acc, [id, { makeAndModel, ...car }]) => ({
-      ...acc,
-      [id]: {
-        ...car,
-        id,
-        title: makeAndModel,
-        subtitle: id
-      }
-    }),
-    {}
-  )
+//   const initialData: Record<string, CarItemData> = Object.entries(cars).reduce(
+//     (acc, [id, { makeAndModel, ...car }]) => ({
+//       ...acc,
+//       [id]: {
+//         ...car,
+//         id,
+//         title: makeAndModel,
+//         subtitle: id
+//       }
+//     }),
+//     {}
+//   )
 
-  useEffect(() => {
-    dispatch(fetchAllCars())
-  }, [])
+//   useEffect(() => {
+//     dispatch(fetchAllCars())
+//   }, [])
 
-  const onAddButtonClick = () => dispatch(openModal({ type: "car" }))
-  const onItemEdit = (id: string) => {
-    dispatch(setEditedCarId(id))
-    onAddButtonClick()
-  }
+//   const onAddButtonClick = () => dispatch(openModal({ type: "car" }))
+//   const onItemEdit = (id: string) => {
+//     dispatch(setEditedCarId(id))
+//     onAddButtonClick()
+//   }
 
-  const onItemDelete = async (id: string) => {
-    const response = await dispatch(deleteCar(id))
+//   const onItemDelete = async (id: string) => {
+//     const response = await dispatch(deleteCar(id))
 
-    if (response.meta.requestStatus === "rejected") {
-      return response.payload as string
-    }
-  }
+//     if (response.meta.requestStatus === "rejected") {
+//       return response.payload as string
+//     }
+//   }
 
-  if (metadata.loading) {
-    return <Loader enableOverlay text="Loading cars data" />
-  }
+//   if (metadata.loading) {
+//     return <Loader enableOverlay text="Loading cars data" />
+//   }
 
-  return (
-    <DataView<CarItemData>
-      filtersConfig={filtersConfig}
-      initialData={initialData}
-      onAddButtonClick={onAddButtonClick}
-      onItemEdit={onItemEdit}
-      onItemDelete={onItemDelete}
-    />
-  )
-}
+//   return (
+//     <DataView<CarItemData>
+//       filtersConfig={filtersConfig}
+//       initialData={initialData}
+//       onAddButtonClick={onAddButtonClick}
+//       onItemEdit={onItemEdit}
+//       onItemDelete={onItemDelete}
+//     />
+//   )
+// }
+
+export const CarsList = () => null
--- packages/react/src/components/core/Modal/CarModalContent.tsx
+++ packages/react/src/components/core/Modal/CarModalContent.tsx
@@ -1,152 +1,154 @@
-import cloneDeep from "lodash.clonedeep"
+// import cloneDeep from "lodash.clonedeep"
 
-import { Form } from "@/components/basic/Form/Form"
-import type { Fields } from "../../basic/Form/Form.models"
+// import { Form } from "@/components/basic/Form/Form"
+// import type { Fields } from "../../basic/Form/Form.models"
 
-import { setCar } from "../../../firebase/cars"
+// import { setCar } from "../../../firebase/cars"
 
-import { useAppSelector } from "@/redux/config"
+// import { useAppSelector } from "@/redux/config"
 
-import { createValidator, getRequiredError } from "../../../utils/validations"
+// import { createValidator, getRequiredError } from "../../../utils/validations"
 
-import type { Car } from "@/shared/models"
+// import type { Car } from "@/shared/models"
 
-const defaultFields: Fields<Car> = {
-  council: {
-    label: "Council",
-    type: "select",
-    options: ["Wolverhampton", "Cornwall", "PSV", "Portsmouth", "Other"],
-    validator: getRequiredError
-  },
-  mot: {
-    label: "MOT",
-    type: "date",
-    validator: getRequiredError
-  },
-  roadTax: {
-    label: "Road Tax",
-    type: "date",
-    validator: getRequiredError
-  },
-  insurance: {
-    label: "Insurance",
-    type: "date",
-    validator: getRequiredError
-  },
-  safetyChecks: {
-    label: "Safety Checks",
-    type: "date",
-    validator: getRequiredError,
-    displayCondition: ({ council }) => council === "PSV"
-  },
-  tachograph: {
-    label: "Tachograph",
-    type: "date",
-    validator: getRequiredError,
-    displayCondition: ({ council }) => council === "PSV"
-  },
-  wheelChairLift: {
-    label: "Wheelchair Lift",
-    type: "toggle",
-    displayCondition: ({ council }) => council === "PSV"
-  },
-  wheelChairLiftCheck: {
-    label: "Wheelchair Lift Check",
-    type: "date",
-    validator: getRequiredError,
-    displayCondition: ({ wheelChairLift }) => wheelChairLift === "Yes"
-  },
-  plateNumber: {
-    label: "Plate Number",
-    type: "number",
-    validator: createValidator({
-      required: true,
-      min: 3,
-      max: 3
-    }),
-    displayCondition: ({ council }) =>
-      council === "Wolverhampton" ||
-      council === "Cornwall" ||
-      council === "Portsmouth"
-  },
-  plateNumberExpiryDate: {
-    label: "Expire Date",
-    type: "date",
-    validator: getRequiredError,
-    displayCondition: ({ council }) =>
-      council === "Wolverhampton" ||
-      council === "Cornwall" ||
-      council === "Portsmouth"
-  },
-  cornwallMot: {
-    label: "Cornwall MOT",
-    type: "date",
-    validator: getRequiredError,
-    displayCondition: ({ council }) => council === "Cornwall"
-  },
-  registrationNumber: {
-    label: "Registration Number",
-    type: "text",
-    validator: createValidator({
-      required: true,
-      min: 7,
-      max: 7,
-      regex: {
-        pattern: /^[A-Z]{2}[0-9]{2}[A-Z]{3}$/,
-        error: "Invalid registration number"
-      }
-    })
-  },
-  makeAndModel: {
-    label: "Make and Model",
-    type: "text",
-    validator: getRequiredError
-  },
-  drivers: {
-    label: "Driver",
-    type: "text",
-    validator: getRequiredError
-  },
-  type: {
-    label: "Type",
-    type: "select",
-    options: ["Wheelchair", "Turism", "MPV", "16 Seater"],
-    validator: getRequiredError
-  },
-  route: {
-    label: "Route",
-    type: "text",
-    validator: getRequiredError
-  }
-}
+// const defaultFields: Fields<Car> = {
+//   council: {
+//     label: "Council",
+//     type: "select",
+//     options: ["Wolverhampton", "Cornwall", "PSV", "Portsmouth", "Other"],
+//     validator: getRequiredError
+//   },
+//   mot: {
+//     label: "MOT",
+//     type: "date",
+//     validator: getRequiredError
+//   },
+//   roadTax: {
+//     label: "Road Tax",
+//     type: "date",
+//     validator: getRequiredError
+//   },
+//   insurance: {
+//     label: "Insurance",
+//     type: "date",
+//     validator: getRequiredError
+//   },
+//   safetyChecks: {
+//     label: "Safety Checks",
+//     type: "date",
+//     validator: getRequiredError,
+//     displayCondition: ({ council }) => council === "PSV"
+//   },
+//   tachograph: {
+//     label: "Tachograph",
+//     type: "date",
+//     validator: getRequiredError,
+//     displayCondition: ({ council }) => council === "PSV"
+//   },
+//   wheelChairLift: {
+//     label: "Wheelchair Lift",
+//     type: "toggle",
+//     displayCondition: ({ council }) => council === "PSV"
+//   },
+//   wheelChairLiftCheck: {
+//     label: "Wheelchair Lift Check",
+//     type: "date",
+//     validator: getRequiredError,
+//     displayCondition: ({ wheelChairLift }) => wheelChairLift === "Yes"
+//   },
+//   plateNumber: {
+//     label: "Plate Number",
+//     type: "number",
+//     validator: createValidator({
+//       required: true,
+//       min: 3,
+//       max: 3
+//     }),
+//     displayCondition: ({ council }) =>
+//       council === "Wolverhampton" ||
+//       council === "Cornwall" ||
+//       council === "Portsmouth"
+//   },
+//   plateNumberExpiryDate: {
+//     label: "Expire Date",
+//     type: "date",
+//     validator: getRequiredError,
+//     displayCondition: ({ council }) =>
+//       council === "Wolverhampton" ||
+//       council === "Cornwall" ||
+//       council === "Portsmouth"
+//   },
+//   cornwallMot: {
+//     label: "Cornwall MOT",
+//     type: "date",
+//     validator: getRequiredError,
+//     displayCondition: ({ council }) => council === "Cornwall"
+//   },
+//   registrationNumber: {
+//     label: "Registration Number",
+//     type: "text",
+//     validator: createValidator({
+//       required: true,
+//       min: 7,
+//       max: 7,
+//       regex: {
+//         pattern: /^[A-Z]{2}[0-9]{2}[A-Z]{3}$/,
+//         error: "Invalid registration number"
+//       }
+//     })
+//   },
+//   makeAndModel: {
+//     label: "Make and Model",
+//     type: "text",
+//     validator: getRequiredError
+//   },
+//   drivers: {
+//     label: "Driver",
+//     type: "text",
+//     validator: getRequiredError
+//   },
+//   type: {
+//     label: "Type",
+//     type: "select",
+//     options: ["Wheelchair", "Turism", "MPV", "16 Seater"],
+//     validator: getRequiredError
+//   },
+//   route: {
+//     label: "Route",
+//     type: "text",
+//     validator: getRequiredError
+//   }
+// }
 
-export const CarModalContent = () => {
-  const { cars, metadata } = useAppSelector(state => state.carsReducer)
-  const { editedCarId } = metadata
-  const car = editedCarId ? cars[editedCarId] : null
+// export const CarModalContent = () => {
+//   const { cars, metadata } = useAppSelector(state => state.carsReducer)
+//   const { editedCarId } = metadata
+//   const car = editedCarId ? cars[editedCarId] : null
 
-  const isEdit = !!car
-  const title = isEdit ? "Edit Car" : "Add Car"
-  const submitLabel = isEdit ? "Update" : "Add"
+//   const isEdit = !!car
+//   const title = isEdit ? "Edit Car" : "Add Car"
+//   const submitLabel = isEdit ? "Update" : "Add"
 
-  const fields = cloneDeep(defaultFields) as Fields<Car>
+//   const fields = cloneDeep(defaultFields) as Fields<Car>
 
-  if (car) {
-    Object.keys(car).forEach(key => {
-      const fieldValue = car[key as keyof Omit<Car, "registrationNumber">]
+//   if (car) {
+//     Object.keys(car).forEach(key => {
+//       const fieldValue = car[key as keyof Omit<Car, "registrationNumber">]
 
-      if (fields[key as keyof Fields<Car>]) {
-        fields[key as keyof Fields<Car>]!.defaultValue = fieldValue
-      }
-    })
-  }
+//       if (fields[key as keyof Fields<Car>]) {
+//         fields[key as keyof Fields<Car>]!.defaultValue = fieldValue
+//       }
+//     })
+//   }
 
-  return (
-    <Form<Car>
-      title={title}
-      submitLabel={submitLabel}
-      fields={fields}
-      action={setCar}
-    />
-  )
-}
+//   return (
+//     <Form<Car>
+//       title={title}
+//       submitLabel={submitLabel}
+//       fields={fields}
+//       action={setCar}
+//     />
+//   )
+// }
+
+export const CarModalContent = () => null
