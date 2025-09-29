090fc11859e36a8359057d1b29c6664afae88047
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-09-29 22:43:42 +0300

    Update wapp webhook post to log req.body into a file

--- packages/express/src/routes/wapp-webhook/post.ts
+++ packages/express/src/routes/wapp-webhook/post.ts
@@ -1,7 +1,18 @@
 import { type Request, type Response } from "express"
+import fs from "fs"
+
+const logToFile = (message: string) => {
+  fs.appendFile("output.txt", message + "\n", err => {
+    if (err) {
+      console.error("Error writing to file:", err)
+    } else {
+      console.log("Message saved to output.txt")
+    }
+  })
+}
 
 export const handleWappWebhookPostRequest = (req: Request, res: Response) => {
-  console.log(req.body)
+  logToFile(JSON.stringify(req.body))
 
   res.status(200).end()
 }
