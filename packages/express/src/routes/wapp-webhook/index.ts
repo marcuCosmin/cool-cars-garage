758a0625cc4a522f1fc5fafc19ab96dacc35234f
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-09-29 22:35:01 +0300

    Register the wapp webhook post request to it's endpoint

--- packages/express/src/routes/wapp-webhook/index.ts
+++ packages/express/src/routes/wapp-webhook/index.ts
@@ -2,8 +2,10 @@ import { Router } from "express"
 import cors from "cors"
 
 import { handleWappWebhookGetRequest } from "./get"
+import { handleWappWebhookPostRequest } from "./post"
 
 export const wappWebhook = Router()
 
 wappWebhook.options("/", cors({ origin: "*" }))
 wappWebhook.get("/", cors({ origin: "*" }), handleWappWebhookGetRequest)
+wappWebhook.post("/", cors({ origin: "*" }), handleWappWebhookPostRequest)
