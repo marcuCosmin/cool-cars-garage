cf24c130217d19ac07f641d19e9b312aedbd9473
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-12-21 16:54:52 +0200

    Add the user invitations deactivation job

---
+++ packages/express/src/jobs/invitations.ts
@@ -0,0 +1,45 @@
+import { firestore } from "@/firebase/config"
+import { getFirestoreDocs } from "@/firebase/utils"
+
+import { getCurrentTimestamp } from "@/utils/get-current-timestamp"
+
+export const clearExpiredInvitations = async () => {
+  const currentTimestamp = getCurrentTimestamp()
+
+  const invitations = await getFirestoreDocs({
+    collection: "invitations",
+    queries: [["isActive", "==", true]]
+  })
+
+  if (!invitations.length) {
+    console.log("No active invitations found.")
+    return
+  }
+
+  let deactivatedCount = 0
+
+  const batch = firestore.batch()
+
+  invitations.forEach(({ creationTimestamp, id }) => {
+    const isExpired =
+      currentTimestamp - creationTimestamp >= 24 * 60 * 60 * 1000
+
+    if (!isExpired) {
+      return
+    }
+
+    const invitationRef = firestore.collection("invitations").doc(id)
+    batch.update(invitationRef, { isActive: false })
+
+    deactivatedCount++
+  })
+
+  if (!deactivatedCount) {
+    console.log("No expired invitations to deactivate.")
+    return
+  }
+
+  console.log(`Deactivating ${deactivatedCount} expired invitations...`)
+  await batch.commit()
+  console.log(`Deactivated ${deactivatedCount} expired invitations!`)
+}
