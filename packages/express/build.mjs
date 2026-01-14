337c8d60c1e0aa7a09d71fdd693eb1fb9cbfed9d
Cosmin Marcu <marcucosmin3@yahoo.com>
2026-01-14 22:10:07 +0200

    Update express to ESM & remove useless build files & update deploy.yml to sync files with the new hosting

--- packages/express/build.mjs
+++ packages/express/build.mjs
@@ -1,12 +1,31 @@
 import * as esbuild from "esbuild"
 
-import { copyUnbundableDeps } from "./copyUnbundableDeps.mjs"
+const isDev = process.env.NODE_ENV !== "production"
 
-await copyUnbundableDeps()
-
-await esbuild.build({
-  entryPoints: ["./src/index.ts", "./src/jobs/**"],
+const buildCommonOptions = {
   bundle: true,
   outdir: "./dist",
-  platform: "node"
-})
+  platform: "node",
+  packages: "external",
+  sourcemap: isDev
+}
+
+const build = async () => {
+  if (isDev) {
+    const ctx = await esbuild.context({
+      entryPoints: ["./src/index.ts"],
+      ...buildCommonOptions,
+      target: "es2020"
+    })
+
+    await ctx.watch()
+    return
+  }
+
+  await esbuild.build({
+    entryPoints: ["./src/index.ts", "./src/jobs/**"],
+    ...buildCommonOptions
+  })
+}
+
+await build()
