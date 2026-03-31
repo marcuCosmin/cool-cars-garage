2a0fa23d06189ad4199d27bd29fb35de61b99114
Cosmin Marcu <marcucosmin3@yahoo.com>
2026-03-31 22:09:23 +0300

    Update handleWappWebhookPostRequest to not return anything

--- packages/express/src/routes/wapp-webhook/post.ts
+++ packages/express/src/routes/wapp-webhook/post.ts
@@ -1,4 +1,5 @@
 import { type Request, type Response } from "express"
 
-export const handleWappWebhookPostRequest = (req: Request, res: Response) =>
+export const handleWappWebhookPostRequest = (req: Request, res: Response) => {
   res.status(200).end()
+}
