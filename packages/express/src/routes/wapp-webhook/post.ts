4645358cbc0f799f07a7b0c3b274c0e29bca0389
Cosmin Marcu <marcucosmin3@yahoo.com>
2026-03-31 16:09:18 +0300

    Remove debug output.txt log from handleWappWebhookPostRequest

--- packages/express/src/routes/wapp-webhook/post.ts
+++ packages/express/src/routes/wapp-webhook/post.ts
@@ -1,18 +1,4 @@
 import { type Request, type Response } from "express"
-import fs from "fs"
 
-const logToFile = (message: string) => {
-  fs.appendFile("output.txt", message + "\n", err => {
-    if (err) {
-      console.error("Error writing to file:", err)
-    } else {
-      console.log("Message saved to output.txt")
-    }
-  })
-}
-
-export const handleWappWebhookPostRequest = (req: Request, res: Response) => {
-  logToFile(JSON.stringify(req.body))
-
+export const handleWappWebhookPostRequest = (req: Request, res: Response) =>
   res.status(200).end()
-}
