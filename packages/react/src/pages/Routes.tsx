2353d71acbc714a523219330f99b206a6ceb6210
Cosmin Marcu <marcucosmin3@yahoo.com>
2026-06-20 21:02:39 +0300

    Comment the current 'Routes' page as it's incomplete and is currently block the react build

--- packages/react/src/pages/Routes.tsx
+++ packages/react/src/pages/Routes.tsx
@@ -1,16 +1,9 @@
-import { DataView } from "@/components/core/DataView/DataView"
-
 export const Routes = () => {
-  const fetchItems = () => {
-    return {}
-  }
-
-  return (
-    <DataView
-      fetchItems={fetchItems}
-      itemMetadataConfig={{}}
-      filtersConfig={{}}
-      openModal={openDataViewModal}
-    />
-  )
+  return null
+  // <DataView
+  //   fetchItems={fetchItems}
+  //   itemMetadataConfig={{}}
+  //   filtersConfig={{}}
+  //   openModal={openDataViewModal}
+  // />
 }
